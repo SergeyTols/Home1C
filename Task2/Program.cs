bb6ceb39237cd52fb62b2ac4c8a0df2f2189ba2f
Sarge <sarge2033@gmail.com>
2022-08-07 19:32:39 +0300

    задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 3 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+  
+if(a > b)
+{   
+    max = a;
+}
+else
+{
+    max = b;
+}
+if(max < c)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное " +max);
