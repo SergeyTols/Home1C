4ad593dabc1123c09e1cea627623ab1ba0058bd4
Sarge <sarge2033@gmail.com>
2022-08-07 21:03:45 +0300

    задание 4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+
+Console.WriteLine("Четные:");
+
+while(a <= N)
+{
+    if(a % 2 == 0) 
+        Console.Write(" " +a);
+        a = a + 1;
+}    
